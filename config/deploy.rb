1765379e9b5c5ce9255f9e34a2601390e35e852d
Jim Suchy <jsuchy@gmail.com>
2012-09-07 21:06:38 -0500

    Deploys clean.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,10 @@
 set :application, "harrisonsuchy.com"
-set :repository,  "git@github.com:jsuchy/harrisonsuchy.com.git"
+set :repository,  "git://github.com/jsuchy/harrisonsuchy.com.git"
 
 set :scm, :git
-set :deploy_to, "/home/jsuchy/harrisonsuchy.com.cap"
+set :deploy_to, "/home/jsuchy/harrisonsuchy.com"
 
 server "harrisonsuchy.com", :app, :web, :db, :primary => true
 
 set :user, "jsuchy"
 set :use_sudo, false
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
